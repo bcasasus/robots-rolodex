f4270a86fe835ea9216df2a07496948b8feeb57b
bcasasus <bernatcasasus97@gmail.com>
2020-06-30 19:13:43 +0200

    Updates

---
+++ precache-manifest.d130166217f7cb51acbe4db2d22879c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a868d54ff6336c597a34506049945293",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "d64a456ab3fa11a3a04c",
+    "url": "/robots-rolodex/static/css/main.d665dc11.chunk.css"
+  },
+  {
+    "revision": "4b4d0534e6a505990af9",
+    "url": "/robots-rolodex/static/js/2.dae51e67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots-rolodex/static/js/2.dae51e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d64a456ab3fa11a3a04c",
+    "url": "/robots-rolodex/static/js/main.c8533a43.chunk.js"
+  },
+  {
+    "revision": "70a12c5377b21966fc44",
+    "url": "/robots-rolodex/static/js/runtime-main.b30d1586.js"
+  }
+]);
